a7d60d3156599fd35bca8f128dea78f6a079e5c2
Daniel Perelman <perelman@cs.washington.edu>
2024-04-29 10:43:21 -0700

    Move testing calls to client mod.

---
+++ client-mod/data.lua
@@ -0,0 +1,22 @@
+require("__ReflectionLibrary__.functions")
+
+log("In ReflectionClient...")
+
+local data_raw = ReflectionLibraryMod.typed_data_raw
+
+local bbook = data_raw['blueprint-book']['blueprint-book']
+bbook:_type_check(true)
+bbook.inventory_size = 42
+bbook:_type_check(true)
+
+for typename, typedValue in pairs(ReflectionLibraryMod.typed_data_raw) do
+  log("Client: Loading typed_data_raw[\""..typename.."\"]...")
+  if not typedValue then
+    log("Client: No entry data.raw[\""..typename.."\"].")
+  else
+    log("Client: Type checking data.raw[\""..typename.."\"]...")
+    if typedValue:_type_check(true) then
+      log("Client: Passed type check: data.raw[\""..typename.."\"]!")
+    end
+  end
+end
