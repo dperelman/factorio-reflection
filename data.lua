3c9c1a466dbe6543c7929b06e87f9024b6a43c1b
Daniel Perelman <perelman@cs.washington.edu>
2024-04-28 17:47:48 -0700

    Fixed bug on abstract prototypes.

--- data.lua
+++ data.lua
@@ -5,7 +5,11 @@ ReflectionLibraryMod.prototypes_by_name = {}
 ReflectionLibraryMod.prototypes_by_typename = {}
 for _, value in ipairs(ReflectionLibraryMod.prototype_api.prototypes) do
   ReflectionLibraryMod.prototypes_by_name[value.name] = value
-  ReflectionLibraryMod.prototypes_by_typename[value.typename] = value
+
+  -- Abstract prototypes don't have typenames.
+  if value.typename then
+    ReflectionLibraryMod.prototypes_by_typename[value.typename] = value
+  end
 end
 
 ReflectionLibraryMod.types_by_name = {}
